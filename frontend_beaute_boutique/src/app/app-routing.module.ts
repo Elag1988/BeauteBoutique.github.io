6f7a4c6b865618b46a8f1181602b74d2cb8927a1
9217-day <dayannevivas@hotmail.com>
2023-05-23 20:44:26 -0500

    carga inicial del proyecto final frontend. Incluye el codigo inicial de las paginas y sus componentes

---
+++ frontend_beaute_boutique/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+const routes: Routes = [
+{
+  path:"",component:LoginComponent
+},
+{
+  path:"home",component:HomeComponent
+},
+{
+  path:"products",component:ProductsComponent
+},
+{
+  path:"about",component:AboutComponent
+},
+{
+  path:"contact",component:ContactComponent
+},
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
