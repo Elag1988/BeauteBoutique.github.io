ca797ff297364f9dbb6356d4259c2ec9a7c91668
Valempz <89655299+Valempz@users.noreply.github.com>
2023-05-29 20:56:14 -0500

    Debido a un error en la carga, se vuelve a hacer la subida de los archivos. La carga inicial de las paginas contacto y acerca de nosotros no sufre cambios.

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit  } from '@angular/core';
+import * as L from 'leaflet';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
-
+export class ContactComponent implements OnInit {
+  ngOnInit() {
+    this.initMap();
+  }
+  private initMap() {
+    const map = L.map('map', {
+      center: [4.60971, -74.08175],
+      zoom: 13,
+      scrollWheelZoom: false
+    });
+  
+    const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+    });
+    tileLayer.addTo(map);
+  
+    L.marker([4.60971, -74.08175]).addTo(map)
+      .bindPopup('¡Hola! ¡Estamos aquí!')
+      .openPopup();
+  }
 }
+
