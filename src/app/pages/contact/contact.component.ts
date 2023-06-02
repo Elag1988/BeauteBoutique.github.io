7c6455dec43a74e53285e427b40d9ed00ba2a41e
Valempz <89655299+Valempz@users.noreply.github.com>
2023-06-01 19:26:02 -0500

    Se hace la carga de actualización en las paginas de Acerca de Nostros y Contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -5,25 +5,8 @@ import * as L from 'leaflet';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
-  ngOnInit() {
-    this.initMap();
-  }
-  private initMap() {
-    const map = L.map('map', {
-      center: [4.60971, -74.08175],
-      zoom: 13,
-      scrollWheelZoom: false
-    });
+export class ContactComponent {
   
-    const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-    });
-    tileLayer.addTo(map);
-  
-    L.marker([4.60971, -74.08175]).addTo(map)
-      .bindPopup('¡Hola! ¡Estamos aquí!')
-      .openPopup();
-  }
 }
+
 
